8a7159fa1c003749a914980ee44a3b52414defe8
ABHINAV REDDY KAITHA <kaitha.abhi@gmail.com>
2019-08-25 15:13:26 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,5 +4,5 @@ from data_processor import DataProcessor
 if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_file = sys.argv[2]
-    dp = DataProcessor(input_dir_path=input_dir)
+    dp = DataProcessor(input_file_name=input_dir)
     dp.generate_report(sys.argv[2])
